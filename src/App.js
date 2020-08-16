d97ea04b02e37722f5116cfe8147feb130226109
Diego Salas <dsalas035@gmail.com>
2020-08-16 02:24:40 -0500

    ui update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ParticlesBg from 'particles-bg';
 import { ToastProvider } from 'react-toast-notifications';
 import Routing from './routes';
 import LanguageButton from './components/Text/LanguageButton';
@@ -6,14 +7,15 @@ import LanguageButton from './components/Text/LanguageButton';
 import './sass/main.scss';
 
 function App() {
-	return (
-		<ToastProvider>
-			<div className='container'>
-				<LanguageButton />
-				<Routing />
-			</div>
-		</ToastProvider>
-	);
+  return (
+    <ToastProvider>
+      <div className="container">
+        <LanguageButton />
+        <Routing />
+        <ParticlesBg color="#2f353e" type="cobweb" bg={true} />
+      </div>
+    </ToastProvider>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,95 +2,89 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CONTACT, LINKEDIN, GITHUB, HOME } from '../../routes/routes';
 import { useTranslation } from 'react-i18next';
+import ParticlesBg from 'particles-bg';
 import SlideMenu from '../../layouts/SlideMenu';
 import Icon from '../../components/Icon';
 
 import './Home.scss';
-import videoParticles from '../../assets/video/particles.mp4';
-import videoParticlesWeb from '../../assets/video/particles.mp4';
 
 export default function Home() {
-	const { t } = useTranslation();
-	const phone = '+527714152997';
-	return (
-		<>
-			<SlideMenu currentPage={HOME} />
-			<div className='landing-home'>
-				<div className='bg-video'>
-					<video className='bg-video__content' autoPlay muted loop>
-						<source src={videoParticles} type='video/mp4' />
-						<source src={videoParticlesWeb} type='video/webm' />
-						Your browser is not supported!
-					</video>
-				</div>
-				<div className='welcome-message'>
-					<h1 className='primary'>
-						<span>{t('HOME.hiIam.1')} </span> Juan Diego Salas Jiménez
-					</h1>
-					<div className='welcome-message__secondary'>
-						<p className='paragraph'>{t('HOME.objetive.1')}</p>
-					</div>
-					<div className='welcome-message__social'>
-						<ul className='social'>
-							<li className='social-item'>
-								<Link to={CONTACT} className='social-item__content'>
-									<Icon icon='at-sign' className='social-item__icon' />
-								</Link>
-							</li>
-							<li className='social-item'>
-								<a
-									href={`tel:${phone}`}
-									className='social-item__content'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<Icon icon='phone' className='social-item__icon' />
-								</a>
-							</li>
-							<li className='social-item'>
-								<a
-									href={`https://api.whatsapp.com/send?phone=${phone}`}
-									className='social-item__content'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<Icon icon='whatsapp' className='social-item__icon' />
-								</a>
-							</li>
-							<li className='social-item'>
-								<a
-									href={LINKEDIN}
-									className='social-item__content'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<Icon icon='linkedin' className='social-item__icon' />
-								</a>
-							</li>
-							<li className='social-item'>
-								<a
-									href={`https://medium.com/@diegosj`}
-									className='social-item__content'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<Icon icon='medium' className='social-item__icon' />
-								</a>
-							</li>
-							<li className='social-item'>
-								<a
-									href={GITHUB}
-									className='social-item__content'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<Icon icon='github' className='social-item__icon' />
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const { t } = useTranslation();
+  const phone = '+527714152997';
+  return (
+    <>
+      <SlideMenu currentPage={HOME} />
+      <div className="landing-home">
+        <div className="welcome-message">
+          <h1 className="primary">
+            <span>{t('HOME.hiIam.1')} </span> Juan Diego Salas Jiménez
+          </h1>
+          <div className="welcome-message__secondary">
+            <p className="paragraph">{t('HOME.objetive.1')}</p>
+          </div>
+          <div className="welcome-message__social">
+            <ul className="social">
+              <li className="social-item">
+                <Link to={CONTACT} className="social-item__content">
+                  <Icon icon="at-sign" className="social-item__icon" />
+                </Link>
+              </li>
+              <li className="social-item">
+                <a
+                  href={`tel:${phone}`}
+                  className="social-item__content"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon icon="phone" className="social-item__icon" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a
+                  href={`https://api.whatsapp.com/send?phone=${phone}`}
+                  className="social-item__content"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon icon="whatsapp" className="social-item__icon" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a
+                  href={LINKEDIN}
+                  className="social-item__content"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon icon="linkedin" className="social-item__icon" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a
+                  href={`https://medium.com/@diegosj`}
+                  className="social-item__content"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon icon="medium" className="social-item__icon" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a
+                  href={GITHUB}
+                  className="social-item__content"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon icon="github" className="social-item__icon" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <ParticlesBg type="lines" bg={true} />
+      </div>
+    </>
+  );
 }
